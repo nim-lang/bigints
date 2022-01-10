480c6be62a30cc188d47d16dbdf7d1e1f82baca3
Dimitri Lesnoff <dimitri.lesnoff@gmail.com>
2022-01-10 17:35:07 +0000

    Fix rc_leftfactorials.nim

--- examples/rc_leftfactorials.nim
+++ examples/rc_leftfactorials.nim
@@ -1,31 +1,37 @@
-import iterutils
 import bigints
 
 const
   one = 1.initBigInt
   zero = 0.initBigInt
 
-proc lfact: iterator: BigInt =
-  result = iterator: BigInt =
-    yield zero
-    var
-      fact = one
-      sum = zero
-      n = one
-    while true:
-      sum += fact
-      fact *= n
-      n += one
-      yield sum
+iterator lfact: BigInt =
+  yield zero
+  var
+    fact = one
+    sum = zero
+    n = one
+  while true:
+    sum += fact
+    fact *= n
+    n += one
+    yield sum
 
-echo "first 11:"
-for i in lfact().slice(last = 10):
-  echo "  ", i
+var i = 0
+for n in lfact():
+  if i == 0:
+    echo "first 11:"
+  if i == 20:
+    echo "20 through 110 (inclusive) by tens:"
+  if i == 1000:
+    echo "Digits in 1,000 through 10,000 (inclusive) by thousands:"
 
-echo "20 through 110 (inclusive) by tens:"
-for i in lfact().slice(20, 110, 10):
-  echo "  ", i
+  if i <= 10:
+    echo i, ": ", n
+  elif i <= 110 and i mod 10 == 0:
+    echo i, ": ", n
+  elif i >= 1000 and i <= 10_000 and i mod 1000 == 0:
+    echo i, ": ", ($n).len
+  elif i > 10_000:
+    break
+  inc i
 
-echo "Digits in 1,000 through 10,000 (inclusive) by thousands:"
-for i in lfact().slice(1_000, 10_000, 1_000):
-  echo "  ", ($i).len
