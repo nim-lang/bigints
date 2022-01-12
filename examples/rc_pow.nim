4b5da3722c8113d83444a7314d73c184d7c135f4
Dimitri Lesnoff <dimitri.lesnoff@gmail.com>
2022-01-12 20:47:31 +0000

    Simplify rc_pow.nim

--- examples/rc_pow.nim
+++ examples/rc_pow.nim
@@ -1,11 +1,8 @@
 # Solution for http://rosettacode.org/wiki/Arbitrary-precision_integers_(included)
-
 import bigints
 import std/math
 
-const five = 5.initBigInt
-
-var x = five ^ (4 ^ (3 ^ 2)).int32
+var x = 5.initBigInt.pow 4 ^ (3 ^ 2)
 var s = $x
 
 echo s[0..19]
