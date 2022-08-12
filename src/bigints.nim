b4a1bebbaeb7570149ff35b406f7a36aa26cdd75
Dimitri Lesnoff <dimitri.lesnoff@gmail.com>
2022-08-12 14:20:50 +0200

    Add a new type and optional parameter to generate random bigints with a fixed number of limbs

--- src/bigints.nim
+++ src/bigints.nim
@@ -66,26 +66,49 @@ else:
 func initBigInt*(val: BigInt): BigInt =
   result = val
 
-proc initRandomBigInt*(nbits: Natural): BigInt =
-  ## Initializes a standalone BigInt with exactly `nbits` bits.
-  let
-    remainder = nbits mod 32
-    n_limbs = (if remainder == 0: nbits shr 5 else: nbits shr 5 + 1)
-    remainingBits  = (if remainder == 0: 32 else: remainder)
-  result.limbs.setLen(n_limbs)
+type
+  SizeDescriptor* = enum
+    Limbs, Bits
 
-  # mask ensures only remainingBits bits can be set to 1
-  # Ensures the first bit is set to 1
-  var
-    mask: uint32 = 0xFFFF_FFFF'u32
-    mask2: uint32 = 0x8000_0000'u32
-  if remainingBits != 32:
-    mask = 1'u32 shl remainingBits - 1
-    mask2 = 1'u32 shl (remainingBits-1)
-  for i in 0 ..< result.limbs.len-1:
-    result.limbs[i] = rand(uint32)
-  let word = rand(uint32)
-  result.limbs[result.limbs.len-1] = word and mask or mask2
+proc initRandomBigInt*(number: Natural, unit: SizeDescriptor = Limbs): BigInt =
+  ## Initializes a standalone BigInt whose value is chosen randomly with exactly
+  ## `number` bits or limbs, depending on the value of `unit`. By default, the 
+  ## BigInt is chosen with `number` limbs chosen randomly.
+  if unit == Limbs:
+    if number == 0:
+      raise newException(ValueError, "A Bigint must have at least one limb !")
+    result.limbs.setLen(number)
+    for i in 0 ..< result.limbs.len-1:
+      result.limbs[i] = rand(uint32)
+    var word = rand(uint32)
+    # Bigint's last limb can be zero, iff there is only one limb
+    # We can't normalize instead, since we need no less than number limbs
+    if number != 1:
+      while word == 0: # Very low probability
+        word = rand(uint32)
+    result.limbs[result.limbs.len-1] = word
+
+  else: # unit == Bits
+    if number == 0:
+      return initBigInt(0)
+    let
+      remainder = number mod 32
+      n_limbs = (if remainder == 0: number shr 5 else: number shr 5 + 1)
+      remainingBits  = (if remainder == 0: 32 else: remainder)
+    result.limbs.setLen(n_limbs)
+    # mask ensures only remainingBits bits can be set to 1
+    # Ensures the first bit is set to 1
+    var
+      mask: uint32 = 0xFFFF_FFFF'u32
+      mask2: uint32 = 0x8000_0000'u32
+    if remainingBits != 32:
+      mask = 1'u32 shl remainingBits - 1
+      mask2 = 1'u32 shl (remainingBits-1)
+    for i in 0 ..< result.limbs.len-1:
+      result.limbs[i] = rand(uint32)
+    let word = rand(uint32)
+    result.limbs[result.limbs.len-1] = word and mask or mask2
+
 
 const
   zero = initBigInt(0)
