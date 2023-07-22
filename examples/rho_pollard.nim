a727fa0f7fc13c9af60dd18f12a784e4483d81e7
Piotr Idzik <65706193+vil02@users.noreply.github.com>
2023-07-22 20:35:34 +0200

    Apply suggestions from code review
    Co-authored-by: konsumlamm <44230978+konsumlamm@users.noreply.github.com>

--- examples/rho_pollard.nim
+++ examples/rho_pollard.nim
@@ -1,46 +1,50 @@
-## Rho-Pollard
+## Pollard's rho algorithm
 ##
-## This file illustrates how to find a factor of an integer using the
-## [rho-Pollard algorithm](https://en.wikipedia.org/wiki/Pollard%27s_rho_algorithm).
+## This file illustrates how to find a factor of an integer using
+## [Pollard's rho algorithm](https://en.wikipedia.org/wiki/Pollard%27s_rho_algorithm).
 
 import bigints
 import std/options
 import std/strformat
 
 
-func rhoPollard(
+func pollardRho(
   n: BigInt,
-  nextIteration: proc(x: BigInt): BigInt {.noSideEffect.},
+  polynomial: proc(x: BigInt): BigInt {.noSideEffect.},
   initialValue: BigInt = 2.initBigInt): Option[BigInt] =
-  ## performs the rho-Pollard search
-  func nextIterationMod(x: BigInt): BigInt =
-    return nextIteration(x) mod n
+  ## Performs Pollard's rho algorithm to find a non-trivial factor of `n`.
+  func polynomialMod(x: BigInt): BigInt =
+    polynomial(x) mod n
+
   var
     turtle = initialValue
     hare = initialValue
     divisor = 1.initBigInt
 
   while divisor == 1.initBigInt:
-    turtle = nextIterationMod(turtle)
-    hare = nextIterationMod(nextIterationMod(hare))
-    divisor = gcd(turtle-hare, n)
+    turtle = polynomialMod(turtle)
+    hare = polynomialMod(polynomialMod(hare))
+    divisor = gcd(turtle - hare, n)
 
   if divisor != n:
-    return some(divisor)
+    some(divisor)
+  else:
+    none(BigInt)
 
 
-func somePoly(number: BigInt): BigInt =
-  return number*number+1.initBigInt
+func somePolynomial(x: BigInt): BigInt =
+  x * x + 1.initBigInt
 
 
 proc main() =
   const someNum = "44077431694086786329".initBigInt
-  let result = rhoPollard(someNum, somePoly)
+  let result = pollardRho(someNum, somePolynomial)
   if result.isSome():
-    echo fmt"{result.get()} is a factor of {someNum}"
-    assert someNum mod result.get() == 0.initBigInt
+    let factor = result.get()
+    echo fmt"{factor} is a factor of {someNum}"
+    assert someNum mod factor == 0.initBigInt
   else:
-    echo fmt"could not find a factor of {someNum} using rho-Pollard algorithm"
+    echo fmt"could not find a factor of {someNum}"
 
 
 main()
